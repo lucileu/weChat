321342b7a8fc3594b66d266678193dd7a90d7fe4
殷杰 <lucileu@yinjiedemacbook-air.local>
2016-03-02 19:08:42 +0800

    初始化了微博项目

---
+++ weChat/weChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weChat
+//
+//  Created by 殷杰 on 16/3/2.
+//  Copyright © 2016年 殷杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weChat/weChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weChat
+//
+//  Created by 殷杰 on 16/3/2.
+//  Copyright © 2016年 殷杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
